4b10fa38723548cea2196e314cff6e32c41fd689
okerivy <okerivy@163.com>
2019-04-21 17:37:48 +0800

    MaterialApp hello下面多两个横线

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(
+void main() =>  runApp(App());
+
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Hello(),
+    );
+  }
+}
+
+class Hello extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
       child: Text(
         "Hello",
         textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 40.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.yellow,
         ),
-    )
-  );
+        ),
+    );
+  }
 }
