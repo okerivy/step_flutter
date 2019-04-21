8883af2fbd6fad0e8e937d33b3959feaa567ad20
okerivy <okerivy@163.com>
2019-04-21 18:05:03 +0800

    列表视图：ListView.builder

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Step_Flutter'),
-          elevation: 0.0, //* 导航栏设置 下面的阴影为0
-        ),
-        body: Hello(),
-      ),
+      home: Home(),
       theme: ThemeData(
         //* 主要样布的颜色
         primarySwatch: Colors.yellow,
@@ -22,7 +16,27 @@ class App extends StatelessWidget {
     );
   }
 }
+// 显示列表
+class Home extends StatelessWidget {
+  Widget _listItemBuilder(BuildContext context, int index) {
+    return Text(posts[index].title);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Step_Flutter'),
+        elevation: 0.0, //* 导航栏设置 下面的阴影为0
+      ),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: _listItemBuilder,
+      ),
+    );
+  }
+}
 
+// 显示hello 字符
 class Hello extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
