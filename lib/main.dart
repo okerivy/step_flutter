aaa155d4cd47b7611a67f7b2924f7b326558e90d
okerivy <okerivy@163.com>
2019-04-21 19:51:08 +0800

    Drawer：在抽屉里使用ListView，DrawerHeade ?? 怎么设置背景色??

--- lib/main.dart
+++ lib/main.dart
@@ -65,16 +65,39 @@ class Home extends StatelessWidget {
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
         ),
-        drawer: Container( //* 默认隐藏的左边的边栏(抽屉), 可以用扫动的手势显示
-          color: Colors.white,
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("这是左边抽屉1"),
-              Text("这是左边抽屉2"),
-            ],
-          ),
+        drawer: ListView( //* 默认隐藏的左边的边栏(抽屉), 可以用扫动的手势显示
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader( //* 头部
+              child: Text("header".toUpperCase()),
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+            ),
+            ListTile(
+              title: Text("Message", textAlign: TextAlign.right,),
+              trailing: Icon( //* 在标签的后面显示一个小图标
+                Icons.message, 
+                color: Colors.lightBlue[200], 
+                size: 22.0,
+              ),
+              // leading: Icon( //* 左边
+              //   Icons.message, 
+              //   color: Colors.lightBlue[200], 
+              //   size: 22.0,
+              // ),
+            ),
+            ListTile(
+              title: Text("Favorite", textAlign: TextAlign.right,),
+              trailing: Icon(Icons.favorite, color: Colors.lightBlue[200], size: 22.0),
+            ),
+            ListTile(
+              title: Text("Settings", textAlign: TextAlign.right,),
+              trailing: Icon(Icons.settings, color: Colors.lightBlue[200], size: 22.0),
+
+            ),
+          ],
+         
         ), 
         // endDrawer: Text("这是右边抽屉"), //* 右边的边栏(抽屉), 可以用扫动的手势显示
       ),
