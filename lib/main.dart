65b01ec7dac08012b82079bdd316c942032162c6
okerivy <okerivy@163.com>
2019-04-21 19:28:05 +0800

    drawer  enddrawer 左右抽屉

--- lib/main.dart
+++ lib/main.dart
@@ -65,6 +65,8 @@ class Home extends StatelessWidget {
             Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
           ],
         ),
+        drawer: Text("这是左边抽屉"), //* 默认隐藏的左边的边栏(抽屉), 可以用扫动的手势显示
+        endDrawer: Text("这是右边抽屉"), //* 右边的边栏(抽屉), 可以用扫动的手势显示
       ),
     );
   }
