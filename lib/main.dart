dd221e01836d540f28f9ef37372dfb1ba0e42c47
okerivy <okerivy@163.com>
2019-04-21 19:12:07 +0800

    TabBar：用标签形式展示内容

--- lib/main.dart
+++ lib/main.dart
@@ -24,25 +24,42 @@ class Home extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100], //* 整体的背景颜色
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          tooltip: "导航条",
-          onPressed: () => debugPrint("导航条按钮 被点击"),
-        ),
-        title: Text('Step_Flutter'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search),
-            tooltip: "搜索",
-            onPressed: () => debugPrint("搜索框 被点击"),
+    //* 标签 Tab 需要三个东西 TabBar TabView TabController
+    return DefaultTabController( //? 1, tabbarController
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100], //* 整体的背景颜色
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: "导航条",
+            onPressed: () => debugPrint("导航条按钮 被点击"),
           ),
-        ],
-        elevation: 0.0, //* 导航栏设置 下面的阴影为0
+          title: Text('Step_Flutter'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: "搜索",
+              onPressed: () => debugPrint("搜索框 被点击"),
+            ),
+          ],
+          elevation: 0.0, //* 导航栏设置 下面的阴影为0
+          bottom: TabBar( //? 2, tabbar
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        body: TabBarView( //? 3, tabbarView
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12),
+          ],
+        ),
       ),
-      body: null,
     );
   }
 }
