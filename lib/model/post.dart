3a793589d151bec3b59ed77d953808bd06805e88
okerivy <okerivy@163.com>
2019-04-21 17:56:43 +0800

    准备假数据

---
+++ lib/model/post.dart
@@ -0,0 +1,90 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+
+  final String title;
+  final String author;
+  final String imageUrl;
+}
+
+
+final List<Post> posts = [
+  Post(
+    title: '英文 Candy Shop',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/candy-shop.jpg',
+  ),
+  Post(
+    title: '英文 Childhood in a picture',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/childhood-in-a-picture.jpg',
+  ),
+  Post(
+    title: '英文 Contained',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/contained.jpg',
+  ),
+  Post(
+    title: '英文 Dragon',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/dragon.jpg',
+  ),
+  Post(
+    title: '英文 Free Hugs',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/free_hugs.jpg',
+  ),
+  Post(
+    title: '英文 Gravity Falls',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/gravity-falls.png',
+  ),
+  Post(
+    title: '英文 Icecream Truck',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/icecreamtruck.png',
+  ),
+  Post(
+    title: '英文 keyclack',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/keyclack.jpg',
+  ),
+  Post(
+    title: '英文 Overkill',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/overkill.png',
+  ),
+  Post(
+    title: '英文 Say Hello to Barry',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/say-hello-to-barry.jpg',
+  ),
+  Post(
+    title: '英文 Space Skull',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/space-skull.jpg',
+  ),
+  Post(
+    title: '英文 The Old Fashioned',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/the-old-fashioned.png',
+  ),
+  Post(
+    title: '英文 Tornado',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/tornado.jpg',
+  ),
+  Post(
+    title: '英文 Undo',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/undo.jpg',
+  ),
+  Post(
+    title: '英文 White Dragon',
+    author: '作者 Mohamed Chahin',
+    imageUrl: 'https://resources.ninghao.org/images/white-dragon.jpg',
+  )
+];
