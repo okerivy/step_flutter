778d6fbe1d415f4e6e185b31f7291beb8a9ae963
okerivy <okerivy@163.com>
2019-04-21 20:12:11 +0800

    .用户帐户头部的背景图像

--- lib/main.dart
+++ lib/main.dart
@@ -63,10 +63,22 @@ class Home extends StatelessWidget {
         drawer: ListView( //* 默认隐藏的左边的边栏(抽屉), 可以用扫动的手势显示
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader( //* 头部
-              child: Text("header".toUpperCase()),
-              decoration: BoxDecoration(
-                color: Colors.grey[100],
+            UserAccountsDrawerHeader( //* 用户头像
+              accountName: Text("V先生", style: TextStyle(fontWeight: FontWeight.bold)),
+              accountEmail: Text("google@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage('https://avatars3.githubusercontent.com/u/4578474?s=400&u=a687cb59490589a80167a9c77758b3f522f91a32&v=4'),
+              ),
+              decoration: BoxDecoration( //* 设置头像背景颜色和背景图片
+                color: Colors.yellow[600],
+                image: DecorationImage(
+                  image: NetworkImage("https://resources.ninghao.org/images/childhood-in-a-picture.jpg"),
+                  fit: BoxFit.cover, //* 图片填充模式
+                  colorFilter: ColorFilter.mode( //* 颜色滤镜 各种模式
+                    Colors.yellow[400].withOpacity(0.5), 
+                    BlendMode.hardLight, 
+                    )
+                )
               ),
             ),
             ListTile(
