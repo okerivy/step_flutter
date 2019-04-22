b0483276b476cde66380fa5fa6b994da14b82635
okerivy <okerivy@163.com>
2019-04-22 09:28:22 +0800

    文字和文字样式

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -2,12 +2,32 @@
 import 'package:flutter/material.dart';
 
 class BasicDemo extends StatelessWidget {
+  final TextStyle _textStyle = TextStyle(
+    fontSize: 16.0,
+  );
+  final String _author = '李白';
+  final String _title = '将进酒';
+
   @override
   Widget build(BuildContext context) {
     return Text(
-      '测试代码 搞一下 \n 测试代码',
-      textAlign: TextAlign.center);
-      
+      '''
+      《 $_title 》 -- $_author
+      君不见，黄河之水天上来，奔流到海不复回。 
+      君不见，高堂明镜悲白发，朝如青丝暮成雪。 
+      人生得意须尽欢，莫使金樽空对月。 
+      天生我材必有用，千金散尽还复来。 
+      烹羊宰牛且为乐，会须一饮三百杯。 
+      岑夫子，丹丘生，将进酒，杯莫停。
+      ''',
+      textAlign: TextAlign.center,
+      style: _textStyle,
+      maxLines: 10,
+      overflow: TextOverflow.ellipsis,
+    );
 
   }
+}
+
+mixin _textStyle {
 }
