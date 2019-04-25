55c7acce0b04f331a42517f207e4b184a04769b7
okerivy <okerivy@163.com>
2019-04-25 08:54:17 +0800

    PageView 创建 页面视图 可以添加多个页面滚动

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -4,7 +4,35 @@ class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("data"),
+      //* 页面视图 可以添加多个页面滚动
+      child: PageView(
+        children: <Widget>[
+          Container(
+            color: Colors.brown[900],
+            alignment: Alignment(0.0, 0.0),
+            child: Text(
+              'ONE',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)
+            ),
+          ),
+          Container(
+            color: Colors.grey[900],
+            alignment: Alignment(0.0, 0.0),
+            child: Text(
+              'TWO',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)
+            ),
+          ),
+          Container(
+            color: Colors.blueGrey[900],
+            alignment: Alignment(0.0, 0.0),
+            child: Text(
+              'THREE',
+              style: TextStyle(fontSize: 32.0, color: Colors.white)
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
