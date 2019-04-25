518df4a04cc36180fa02427ca4a9ba2f811e21d2
okerivy <okerivy@163.com>
2019-04-25 09:10:29 +0800

    PageView 属性

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -4,8 +4,14 @@ class ViewDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      //* 页面视图 可以添加多个页面滚动
+      //* PageView 页面视图 可以添加多个页面滚动
       child: PageView(
+        pageSnapping: false, //? 页面自动分页效果
+        reverse: true, //? 页面顺序反转
+        scrollDirection: Axis.vertical, //? 页面滚动 方向
+        //? 子页面在可视视图 滚动到中间显示的时候  会触发回调
+        onPageChanged: (currentIndex) => debugPrint("滚动 $currentIndex"),
+        
         children: <Widget>[
           Container(
             color: Colors.brown[900],
