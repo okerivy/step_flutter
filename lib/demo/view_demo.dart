a77a8e9f9b100dd68f1b088cea7106feb2684894
okerivy <okerivy@163.com>
2019-04-25 09:11:49 +0800

    用 PageController 调整页面的显示

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -11,7 +11,13 @@ class ViewDemo extends StatelessWidget {
         scrollDirection: Axis.vertical, //? 页面滚动 方向
         //? 子页面在可视视图 滚动到中间显示的时候  会触发回调
         onPageChanged: (currentIndex) => debugPrint("滚动 $currentIndex"),
-        
+        //* PageController 可以用来控制 PageView 小部件 页面显示的一个对象
+        controller: PageController(
+          initialPage: 1, //? 初始时 显示哪个页面
+          // Fixme: 切换tab 时, 页面状态没有保留??? 是因为是 StatelessWidget??
+          keepPage: false, //? 默认会记住用户滚动到哪个页面
+          viewportFraction: 0.8, //? 页面占用 可视区域的比例 (根据滚动方向)
+        ),
         children: <Widget>[
           Container(
             color: Colors.brown[900],
