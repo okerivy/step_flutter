cea7eaa4922e2a7f67b32bb5de31d83aa268f257
okerivy <okerivy@163.com>
2019-04-26 09:08:46 +0800

    抽取 SliverGridDemo

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -11,32 +11,39 @@ class SliverDemo extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          //? SliverGrid 和 GridView 差不多
-          //? 其实 GridView 就相当于 CustomScrollView 的 slivers 只加了一个 SliverGrid
-          SliverGrid(
-            //* gridDelegate 用来 构建网格视图相关的一些东西
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
-              childAspectRatio: 1.6, //? 宽高比
-            ),
-
-            //* delegate 用来构建网格视图中的项目用的
-            delegate: SliverChildBuilderDelegate(
-              //? 需要提供一个 build 方法, 这里就直接添加到里面了
-              (BuildContext context, int index) {
-                return Container(
-                  child: Image.network(
-                    posts[index].imageUrl,
-                    fit: BoxFit.cover,
-                  ),
-                );
-              },
-              childCount: posts.length, //? 网格视图的 item数量
-            ),
-          ),
+          SliverGridDemo(),
         ],
+      ),
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //? SliverGrid 和 GridView 差不多
+    //? 其实 GridView 就相当于 CustomScrollView 的 slivers 只加了一个 SliverGrid
+    return SliverGrid(
+      //* gridDelegate 用来 构建网格视图相关的一些东西
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        childAspectRatio: 1.6, //? 宽高比
+      ),
+
+      //* delegate 用来构建网格视图中的项目用的
+      delegate: SliverChildBuilderDelegate(
+        //? 需要提供一个 build 方法, 这里就直接添加到里面了
+        (BuildContext context, int index) {
+          return Container(
+            child: Image.network(
+              posts[index].imageUrl,
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        childCount: posts.length, //? 网格视图的 item数量
       ),
     );
   }
