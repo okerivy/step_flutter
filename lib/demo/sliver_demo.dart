46db34b475e4679a0911ee7a8687c1c7681eb07b
okerivy <okerivy@163.com>
2019-04-26 08:48:51 +0800

    //? Sliver 相当于 在应用页面视口 ViewPort 切开一小块 //? 一般我们会把 Sliver 放在一个 CustomScrollView 的Sliver 属性里面

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -1,10 +1,18 @@
 
 import 'package:flutter/material.dart';
 
+//? Sliver 相当于 在应用页面视口 ViewPort 切开一小块
+//? 一般我们会把 Sliver 放在一个 CustomScrollView 的Sliver 属性里面
 class SliverDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //* Scaffold提供了一个界面的结构
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          
+        ],
+      ),
+    );
   }
 }
