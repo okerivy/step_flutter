df5fa6736946c1b5413ae49e58aba61d16c2a039
okerivy <okerivy@163.com>
2019-04-26 18:34:52 +0800

    TextField 文本字段

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -13,9 +13,29 @@ class FormDemo extends StatelessWidget {
         data: Theme.of(context).copyWith(
           primaryColor: Colors.lightBlue,
         ),
-        child: ThemeDemo(),
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFieldDemo(),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class TextFieldDemo extends StatefulWidget {
+  @override
+  _TextFieldDemoState createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
   }
 }
 
