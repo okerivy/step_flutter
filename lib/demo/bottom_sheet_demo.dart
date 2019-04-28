ca4799676a609ed67734e8726d27f4e8b2969603
okerivy <okerivy@163.com>
2019-04-28 18:00:12 +0800

    GlobalKey<ScaffoldState> //? BottomSheet 是中屏幕底部滑动显示出来的一个东西 //? 一般触发显示 BottomSheet ,可以使用 ScaffoldState 的 showBottomSheet 方法 //? 要使用这个方法, 一般我们可以创建一个 GlobleKey, 用它可以引用那个对应的 Scaffold

--- lib/demo/bottom_sheet_demo.dart
+++ lib/demo/bottom_sheet_demo.dart
@@ -6,9 +6,39 @@ class BottomSheetDemo extends StatefulWidget {
 }
 
 class _BottomSheetDemoState extends State<BottomSheetDemo> {
+  //? BottomSheet 是中屏幕底部滑动显示出来的一个东西
+  //? 一般触发显示 BottomSheet ,可以使用 ScaffoldState 的 showBottomSheet 方法
+  //? 要使用这个方法, 一般我们可以创建一个 GlobleKey, 用它可以引用那个对应的 Scaffold
+
+  final _bottomSheetScaffoldKey = GlobalKey<ScaffoldState>();
+
+  _openBottomSheet() {
+    _bottomSheetScaffoldKey
+      .currentState
+      .showBottomSheet((BuildContext context) {
+        return BottomAppBar(
+          child: Container(
+            height: 90.0,
+            width: double.infinity,
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              children: <Widget>[
+                Icon(Icons.pause_circle_outline),
+                SizedBox(width: 16.0),
+                Text('01:30 / 03:30'),
+                Expanded(
+                  child: Text('Fix you -coldplay', textAlign: TextAlign.right,),
+                )
+              ],
+            ),
+          ),
+        );
+      });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _bottomSheetScaffoldKey,
       appBar: AppBar(
         title: Text('BottomSheetDemo'),
         elevation: 0.0,
@@ -22,6 +52,11 @@ class _BottomSheetDemoState extends State<BottomSheetDemo> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text('BottomSheetDemo 文本'),
+                SizedBox(width: 16.0),
+                FlatButton(
+                  child: Text('Open BottomSheet'),
+                  onPressed: _openBottomSheet,
+                )
               ],
             )
           ],
