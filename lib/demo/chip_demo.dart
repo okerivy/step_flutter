e95717a306e08e24e26090e121c31e7686364a40
okerivy <okerivy@163.com>
2019-04-30 08:35:52 +0800

    Chip 小碎片

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -22,7 +22,33 @@ class _ChipDemoState extends State<ChipDemo> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text('ChipDemo 文本'),
+                Chip(
+                  label: Text('Life'),
+                ),
+                SizedBox(width: 16.0),
+                Chip(
+                  label: Text('Sunset'),
+                  backgroundColor: Colors.orange,
+                ),
+                SizedBox(width: 16.0),
+                Chip(
+                  label: Text('LU YA'),
+                  //? 符号 头像
+                  avatar: CircleAvatar(
+                    backgroundColor: Colors.lightBlue,
+                    child: Text('陆'),
+                  ),
+                ),
+                SizedBox(width: 16.0),
+                Chip(
+                  label: Text('V for Vendetta'),
+                  //? 符号 头像
+                  avatar: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      'https://avatars3.githubusercontent.com/u/4578474?s=400&u=a687cb59490589a80167a9c77758b3f522f91a32&v=4'
+                    ),
+                  ),
+                ),
               ],
             )
           ],
