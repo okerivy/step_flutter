0652cb1335be5768183bbb00b9e38c0d237166c7
okerivy <okerivy@163.com>
2019-04-30 18:19:29 +0800

    数据表格的排序 改成 StatefulWidget 方法

--- lib/demo/data_table_demo.dart
+++ lib/demo/data_table_demo.dart
@@ -23,12 +23,22 @@ class _DataTableDemoState extends State<DataTableDemo> {
 }
 
 //? 通过列表生成数据表格
-class DataTableDemoTestFromPosts extends StatelessWidget {
+class DataTableDemoTestFromPosts extends StatefulWidget {
+  @override
+  _DataTableDemoTestFromPostsState createState() => _DataTableDemoTestFromPostsState();
+}
+
+class _DataTableDemoTestFromPostsState extends State<DataTableDemoTestFromPosts> {
+  int _sortColumnIndex = 0;
+  bool _sortAscending = true;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: <Widget>[
         DataTable(
+          sortColumnIndex: _sortColumnIndex,
+          sortAscending: _sortAscending, //? true 升序列
           //? 栏目
           columns: [
             DataColumn(
@@ -38,6 +48,26 @@ class DataTableDemoTestFromPosts extends StatelessWidget {
               //   child: Text("Title"),
               // )
               label: Text("Title"),
+              onSort: (int index, bool ascending) {
+                setState(() {
+                  _sortColumnIndex = index;
+                  _sortAscending = ascending;
+                  print('index $index, ascending $ascending');
+
+                  posts.sort((a, b) {
+                    if (!ascending) {
+                      final c = a;
+                      a = b;
+                      b = c;
+                    }
+                    // sort 的交换是按 升序排的 
+                    // 因为是升序, 如果 a.len < b.len 符合sort升序规则, 不交换
+                    // 如果是降序, 因为 a.len < b.len 符合sort升序规则, 不交换, 
+                    //    但是 因为是降序, 需要交换, 所以 先搞 两个假数据, 符合交换规则, 所以就先让 a 和 b 交换大小了, 然后根据 假a 假b 来判断交换
+                    return a.title.length.compareTo(b.title.length);
+                  });
+                });
+              }
             ),
             DataColumn(
               label: Text("Author"),
@@ -62,6 +92,50 @@ class DataTableDemoTestFromPosts extends StatelessWidget {
     );
   }
 }
+
+// //? 通过列表生成数据表格
+// class DataTableDemoTestFromPosts extends StatelessWidget {
+  
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListView(
+//       children: <Widget>[
+//         DataTable(
+//           sortColumnIndex: _sortColumnIndex,
+//           sortAscending: _sortAscending, //? true 升序列
+//           //? 栏目
+//           columns: [
+//             DataColumn(
+//               //? 可以通过 Container 设置占用的宽度
+//               // label: Container(
+//               //   width: 150,
+//               //   child: Text("Title"),
+//               // )
+//               label: Text("Title"),
+//             ),
+//             DataColumn(
+//               label: Text("Author"),
+//             ),
+//             DataColumn(
+//               label: Text("Image"),
+//             ),
+//           ],
+//           rows: posts.map(
+//             (Post item) {
+//               return DataRow(
+//                 cells: [
+//                   DataCell(Text(item.title)),
+//                   DataCell(Text(item.author)),
+//                   DataCell(Image.network(item.imageUrl))
+//                 ]
+//               );
+//             }
+//           ).toList(),
+//         ),
+//       ],
+//     );
+//   }
+// }
 
 //? 手动生成测试数据
 class DataTableDemoTest01 extends StatelessWidget {
