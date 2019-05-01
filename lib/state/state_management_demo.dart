48eba28fcdc69abadf46c0b52d8718930e1fea15
okerivy <okerivy@163.com>
2019-05-01 13:57:50 +0800

    InheritedWidget 直接把数据传递给需要的小部件 `_Inherited` 通过 `InheritedWidget` 传递数据

--- lib/state/state_management_demo.dart
+++ lib/state/state_management_demo.dart
@@ -23,12 +23,15 @@ import 'package:flutter/material.dart';
 class StateManagementDemo extends StatefulWidget {
   
   @override
-  _StateManagementDemoState createState() => _StateManagementDemoState();
+  // _StateManagementDemoState createState() => _StateManagementDemoState();
+  _StateManagementDemoState_Inherited createState() => _StateManagementDemoState_Inherited();
 }
 
 /**
  * 在这个 `State` 类里面,可以包含 `StateManagementDemo` 里面的小部件的一些数据 
  * 和 小部件表示的界面 : 也就是 `build` 方法返回的东西
+ * 
+ * `_nomal` 表示 一层一层 往下传
  */
 class _StateManagementDemoState extends State<StateManagementDemo> {
   //? 内部属性 需要加 下划线 _
@@ -62,11 +65,53 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
 }
 
 /**
+ * `_Inherited` 通过 `InheritedWidget` 传递数据
+ */
+class _StateManagementDemoState_Inherited extends State<StateManagementDemo> {
+  //? 内部属性 需要加 下划线 _
+  int _count = 0;
+  void _increaseCount() {
+    setState(() {
+      _count += 1;
+    });
+    print('_Inherited 这是儿子内部点击: count = $_count ');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //? 给 Scaffold 添加一个新的包装
+    /// 这样 `CounterProvider` `小部件树`下的`所有小部件`都会得到这个 `count` 和 `increaseCount`
+    return CounterProvider(
+      count: _count,
+      increaseCount: _increaseCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('StateManagementDemo'),
+          elevation: 0.0,
+        ),
+        body: ConterWrapperDemo_Inherited(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              _count += 1;
+            });
+            print('_Inherited count = $_count 变化,调用 setState方法后 页面也变化');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+/**
  * 现在数据传递的方向是 
  * `StateManagementDemo` -> `ConterWrapperDemo` -> `CounterDemo`
  * 因为 子部件 `CounterDemo`需要数据, 所以 这样一级 一级 往下传,有个问题, 就是 
  * `ConterWrapperDemo` 其实并不需要 这些数据: `count` 和  `increaseCount`
  * 如果层级很深, 就会很麻烦
+ * 
+ * `_nomal` 表示 一层一层 往下传
  */
 class ConterWrapperDemo extends StatelessWidget {
   final int count;
@@ -82,10 +127,24 @@ class ConterWrapperDemo extends StatelessWidget {
 }
 
 /**
+ * `_Inherited` 通过 `InheritedWidget` 传递数据
+ */
+class ConterWrapperDemo_Inherited extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CounterDemo_Inherited(),
+    );
+  }
+}
+
+/**
  * `StateManagementDemo` 小部件的状态 是[自己管理]的, 有时候小部件的状态,可以由[父辈管理]
  * 下面我们把 `body`抽取出来 变成 `CounterDemo`, 这样 `StateManagementDemo` 就是`父辈`, `CounterDemo` 就是`儿子`
  * 儿子`CounterDemo` 的状态 由 父亲 `StateManagementDemo` 管理, 当数据[状态]变化时,小部件会重新创建
  * [注意:] 重建的小部件 这里是指 父亲`StateManagementDemo` , 因为 `setState` 这个方法在父亲那里.
+ * 
+ * `_nomal` 表示 一层一层 往下传
  */
 class CounterDemo extends StatelessWidget {
   final int count;
@@ -104,6 +163,52 @@ class CounterDemo extends StatelessWidget {
       onPressed: increaseCount,
     );
   }
+}
+
+/**
+ * `_Inherited` 通过 `InheritedWidget` 传递数据
+ */
+class CounterDemo_Inherited extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int count = CounterProvider.of(context).count;
+    final VoidCallback increaseCount = CounterProvider.of(context).increaseCount;
+    return ActionChip(
+      label: Text('$count'),
+      //? 执行的是从 爸爸那里传递过来的回调, 回调的 方法体 在爸爸 哪里.
+      onPressed: increaseCount,
+    );
+  }
+}
+
+
+
+/**
+ * `CounterProvider: CounterDemo 数据的提供者`
+ * 在这个类里面 可以设置下数据, 这些数据, 都必须是 `final`的, 不能变化.
+ */
+class CounterProvider extends InheritedWidget {
+  final int count;
+  final VoidCallback increaseCount;
+  final Widget child;
+
+  CounterProvider({
+    this.count, 
+    this.increaseCount, 
+    this.child
+  }):super(child: child);
+
+  //? 类方法 静态方法, 用它可以得到小部件里面就state, 也就是数据 count 和 increaseCount
+  static CounterProvider of(BuildContext context) => 
+    context.inheritFromWidgetOfExactType(CounterProvider);
+
+  @override
+  //? 决定是否通知: 继承自这个小部件的 小部件
+  //? 它这个小部件重建以后, 有的时候我们需要 重建 继承这个小部件的 小部件, 有的时候 不需要
+  //? 如果这个小部件的数据 和 oldWidget 数据一样,就不需要重建
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    return true;
+  }  
 }
 
 /**
