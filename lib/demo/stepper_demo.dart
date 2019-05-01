3b54731269a1759cf3bb6188db404ed499cfc918
okerivy <okerivy@163.com>
2019-05-01 11:34:33 +0800

    Stepper 步骤 用Theme 来覆盖掉 应用的部分主题的配置

--- lib/demo/stepper_demo.dart
+++ lib/demo/stepper_demo.dart
@@ -18,11 +18,33 @@ class _StepperDemoState extends State<StepperDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('StepperDemo 文本'),
-              ],
+            //? 用Theme 来覆盖掉 应用的部分主题的配置
+            Theme(
+              data: Theme.of(context).copyWith(
+                primaryColor: Colors.green,
+              ),
+              child: Stepper(
+                steps: [
+                  Step(
+                    title: Text('Login'),
+                    subtitle: Text('Login first'),
+                    content: Text('Ut ullamco minim excepteur dolor amet proident ea deserunt.'),
+                    isActive: true,
+                  ),
+                  Step(
+                    title: Text('Choose Plan'),
+                    subtitle: Text('Choose you plan'),
+                    content: Text('Duis fugiat adipisicing ex cupidatat amet ut deserunt sint occaecat ullamco dolor dolor eu.'),
+                    isActive: true,
+                  ),
+                  Step(
+                    title: Text('Confirm payment'),
+                    subtitle: Text('Comfirm you payment method.'),
+                    content: Text('Culpa fugiat anim amet commodo pariatur proident Lorem.'),
+                    isActive: true,
+                  ),
+                ],
+              ),
             )
           ],
         ),
