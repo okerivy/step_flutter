a452a785332e1c40f13bf6e0afc17650c2e1bb91
okerivy <okerivy@163.com>
2019-05-01 19:03:19 +0800

    创建 空白演示文件  BlockDemo BLoC == BusinessLogicComponent 业务逻辑组件 会用到 Dart里面 的Stream 或者RxDart 的 Observerable

---
+++ lib/bloc/bloc_demo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BlockDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BlockDemo'),
+        elevation: 0.0,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:step_flutter/bloc/bloc_demo.dart';
 import 'package:step_flutter/demo/basic_demo.dart';
 import 'package:step_flutter/demo/form_demo.dart';
 import 'package:step_flutter/demo/hello_demo.dart';
@@ -25,7 +26,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false, //* 去掉手机上的 Debug图标
       // home: NavigatorDemo(),
       // * 默认的根路由是 home == '/', 但是 可以通过 initialRoute 来定义app启动时进入的初始路由 (可以pop到根路由)
-      initialRoute: '/rxdart',
+      initialRoute: '/bloc',
       //? 注册路由, 根据名字来跳转
       routes: {
         '/': (context) => Home(),
@@ -36,6 +37,7 @@ class App extends StatelessWidget {
         '/state-management': (context) => StateManagerDemo(),
         '/stream': (context) => StreamDemo(),
         '/rxdart': (context) => RxDartDemo(),
+        '/bloc': (context) => BlockDemo(),
       },
       //* 这里定义的主题在整个App 都是有效的
       theme: ThemeData(
