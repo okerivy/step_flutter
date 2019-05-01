b241a15cf079d39fedea1887b88354259fc2a251
okerivy <okerivy@163.com>
2019-05-01 10:56:00 +0800

    设置一个卡片  Card

--- lib/demo/card_demo.dart
+++ lib/demo/card_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:step_flutter/model/post.dart';
 
 class CardDemo extends StatefulWidget {
   @override
@@ -15,16 +16,52 @@ class _CardDemoState extends State<CardDemo> {
       ),
       body: Container(
         padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text('CardDemo 文本'),
-              ],
-            )
-          ],
+        child: ListView(
+          children: posts.map((post) {
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 16/9,
+                    child: Image.network(
+                      post.imageUrl,
+                      fit: BoxFit.cover
+                    ),
+                  ),
+                  ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(post.imageUrl),
+                    ),
+                    title: Text(post.title),
+                    subtitle: Text(post.author),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      post.description,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                  Divider(height: 0,),
+                  ButtonTheme.bar(
+                    child: ButtonBar(
+                      children: <Widget>[
+                        FlatButton(
+                          child: Text('Like'.toUpperCase()),
+                          onPressed: () {},
+                        ),
+                        FlatButton(
+                          child: Text('Read'.toUpperCase()),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
--- lib/model/post.dart
+++ lib/model/post.dart
@@ -32,6 +32,7 @@ final List<Post> posts = [
     title: '英文 Contained',
     author: '作者 Mohamed Chahin',
     imageUrl: 'https://resources.ninghao.org/images/contained.jpg',
+    description: 'Esse ut nulla velit reprehenderit veniam sint nostrud nulla exercitation ipsum. Officia deserunt aliquip aliquip excepteur eiusmod dolor. Elit amet ipsum labore sint occaecat dolore tempor officia irure voluptate ad. Veniam laboris deserunt aute excepteur sit deserunt dolor esse dolor velit sint nulla anim ut. Reprehenderit voluptate adipisicing culpa magna ea nulla ullamco consectetur. Cupidatat adipisicing consequat adipisicing sit consectetur dolor occaecat.',
   ),
   Post(
     title: '英文 Dragon',
