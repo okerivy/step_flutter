a8cb9aa1b67198352d3d7d5d0f84b77d762cf159
okerivy <okerivy@163.com>
2019-05-01 11:19:27 +0800

    对卡片 上图片圆角效果 裁剪的两种方法 1, clipBehavior: Clip.antiAlias, 直接裁剪 最好的方法 2, 给图片包装一个  ClipRRect 给他的孩子裁剪出圆角效果

--- lib/demo/card_demo.dart
+++ lib/demo/card_demo.dart
@@ -19,13 +19,29 @@ class _CardDemoState extends State<CardDemo> {
         child: ListView(
           children: posts.map((post) {
             return Card(
+              //?  shape 功能也很强大, 可以剪切成圆形
+              // shape: CircleBorder(side: const BorderSide(
+              //     color: Colors.green,
+              //     width: 2.0,
+              //     style:BorderStyle.solid
+              // )),
+              // borderOnForeground: false, 
+              //? 方法1 对图片进行剪切  因为图片默认不剪切, 所以上面两个角没有剪切
+              clipBehavior: Clip.antiAlias,
               child: Column(
                 children: <Widget>[
                   AspectRatio(
                     aspectRatio: 16/9,
-                    child: Image.network(
-                      post.imageUrl,
-                      fit: BoxFit.cover
+                    //? 方法2 对图片添加 圆角效果 ClipRRect 给他的孩子裁剪出圆角效果
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(4.0),
+                        topRight: Radius.circular(4.0),
+                      ),
+                      child: Image.network(
+                        post.imageUrl,
+                        fit: BoxFit.cover
+                      ),
                     ),
                   ),
                   ListTile(
