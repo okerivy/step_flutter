da616816ea27f378f86ac395c579ff180e2333c8
okerivy <okerivy@163.com>
2019-05-01 21:54:54 +0800

    json 转换:  dartconvert

--- lib/http/http_demo.dart
+++ lib/http/http_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert' as convert;
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http; //? 起个名字??
 import 'dart:async';
@@ -26,7 +28,25 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   @override
   void initState() { 
     super.initState();
-    fetchPost();
+    // fetchPost();
+
+    //? map 类型的数据 Map<String, String> 
+    final post = {
+      'title': 'hello ~',
+      'description': 'nice to meet you',
+    };
+    print(post);
+    print(post['title']);
+    print(post['description']);
+
+    //? map 转 json [String postJson]  可以发送到应用的后台
+    final postJson = convert.json.encode(post);
+    print(postJson);
+
+    //? 请求接口返回的数据, 需要转换以后才能在fltter 中使用
+    //? json 转成 map
+    final postJsonConverted = convert.json.decode(postJson);
+    print(postJsonConverted is Map);
   }
 
   fetchPost() async {
