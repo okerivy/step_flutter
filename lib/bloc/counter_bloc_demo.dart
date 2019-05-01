1860be1cb1c86394caa902fa7fb2de9fb1dc9197
okerivy <okerivy@163.com>
2019-05-01 19:33:04 +0800

    创建和使用 BLoC, 用Sink 输入数据

--- lib/bloc/counter_bloc_demo.dart
+++ lib/bloc/counter_bloc_demo.dart
@@ -1,5 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
+/**
+ * 按下 CounterActionButton 这个漂浮按钮, 可以通过 Sink的add 方法,往Stream 上添加一个数据
+ * BLoC 会发布这个数据 到 指定的Stream上面
+ * 
+ * 界面上的这个 ActionChip 可以用 StreamBuilder 来生成 ,他会接收一个  Stream, 
+ * 当Stream 上面的数据有变化的时候, 会重新构建这个小部件
+ */
 class CounterHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -10,7 +19,8 @@ class CounterHome extends StatelessWidget {
       child: ActionChip(
         label: Text('0'),
         onPressed: () {
-          _counterBloc.log();
+          // _counterBloc.log();
+          _counterBloc.counter.add(1);
         },
       ),
     );
@@ -26,8 +36,9 @@ class CounterActionButton extends StatelessWidget {
     return FloatingActionButton(
       child: Icon(Icons.add), 
       onPressed: () {
-        _counterBloc.log();
-      },
+        // _counterBloc.log();
+        _counterBloc.counter.add(1);
+      }
     );
   }
 }
@@ -54,6 +65,24 @@ class CounterProvider extends InheritedWidget {
 
 
 class CounterBloc {
+  final _counterActionController = StreamController<int>();
+  //? 这样在我们的小部件里面就可以获取这个 sink, 通过 sink 往Stream 上面添加数据
+  StreamSink<int> get counter => _counterActionController.sink;
+
+  CounterBloc() {
+    _counterActionController.stream.listen(onData);
+  }
+
+  void onData(int data) {
+    print('$data');
+  }
+
+  @override
+  void dispose() { 
+    _counterActionController.close();
+
+  }
+
   void log() {
     print('Bloc');
   }
