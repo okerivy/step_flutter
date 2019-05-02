8703ef5452e81ef578c7457fa5ddad826b97fa63
okerivy <okerivy@163.com>
2019-05-02 09:48:19 +0800

    fromJson 准备的说应该是把map 转成model,而不是服务器返回的原始json

--- lib/http/http_demo.dart
+++ lib/http/http_demo.dart
@@ -1,7 +1,8 @@
 import 'dart:convert' as convert;
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http; //? 起个名字??
+//? 起个名字?? 以前是调用 get, 现在需要用 http.get 可能是为了好看
+import 'package:http/http.dart' as http; 
 import 'dart:async';
 
 
@@ -75,10 +76,28 @@ class Post {
     this.description,
   );
 
-  // Fixme: 冒号 这是什么写法?
-  //? 构造函数大括号前还可用冒号指明初始化字段表，常用来设置final字段
-  // http://wiki.jikexueyuan.com/project/dart-language-tour/classes.html
+ 
+  /**
+   *  // Fixme: 冒号 这是什么写法?
+   * http://wiki.jikexueyuan.com/project/dart-language-tour/classes.html
+   * 初始化列表
+   * 除了调用父类构造函数，你也可以在构造函数体运行之前初始化实例变量。用逗号隔开使其分别初始化。
+   * 
+   * http://nucode.cn/article/1543745957253?p=1&m=0
+   * 注意：在实例变量中只可以用 final 而非 const。必须在构造函数体之前初始化 final 变量
+   * 
+   * http://flycode.co/archives/280160
+   * 构造函数大括号前还可用冒号指明初始化字段表，常用来设置final字段
+   *
+   * 
+   * Post是一个默认的构造函数
+   * 使用命名构造函数可以为一个类声明多个构造函数，或者说是提供额外的声明
+   * Post.fromJson 是一个 命名的构造函数
+   */
+  //? 把json 转成 Model, 准备的说应该是把map 转成model,而不是服务器返回的原始json
   Post.fromJson(Map json)
     : title = json['title'],
       description = json['description'];
+  
+  
 }
