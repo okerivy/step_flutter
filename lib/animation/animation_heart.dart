f5cb7497904620abe2f5841ab8746001f36078fa
okerivy <okerivy@163.com>
2019-05-03 10:33:58 +0800

    animationController 动画控制器

--- lib/animation/animation_heart.dart
+++ lib/animation/animation_heart.dart
@@ -19,7 +19,41 @@ class AnimationHeartHome extends StatefulWidget {
   _AnimationHeartHomeState createState() => _AnimationHeartHomeState();
 }
 
-class _AnimationHeartHomeState extends State<AnimationHeartHome> {
+class _AnimationHeartHomeState extends State<AnimationHeartHome> with TickerProviderStateMixin{
+  AnimationController animationHeartController;
+
+  @override
+  void initState() { 
+    super.initState();
+
+    /// 我们 还需要 给 `animationController` 提供一个 `vsync` 属性
+    /// 主要作用就是 `防止屏幕外的动画 消耗不必要的资源`
+    /// 这个 vsync 应该是一个  `TickerProvider`
+    /// `TickerProvider 会在每一帧做出一个反应 `
+    /// 比如通知一下动画对象计算出当前帧的值
+    /// `frame 就是帧`, 一帧可以理解成一个画面, 一般 1秒可以显示 60帧,
+    /// 也就是 1秒的动作可以分解成 60个画面
+    animationHeartController = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      //? 这样我们就可以把 vsync设置成当前对象  this
+      vsync: this,
+    );
+
+    int index = 0;
+    animationHeartController.addListener((){
+      index += 1;
+      print('$index : ${animationHeartController.value}');
+    });
+
+    animationHeartController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationHeartController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
